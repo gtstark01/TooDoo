087344cfd94b6392b2f59c26a77e1522560cdde8
Garrett Stark <gtstark07@gmail.com>
2025-06-09 02:21:22 +0000

    Upload New File

---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type = string
+  default = "clgcporg10-153"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = string
+  default = "us-central1-a"
+}
+
+variable "my_home_ip_cidr" {
+  type = string
+  default = "72.198.103.130/32"
+}
